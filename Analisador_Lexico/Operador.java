c8f399c628bd54bc0d3aa184240fa88f45269319
Pedro Lucas Adorno <Pedrolucasadorno254@gmail.com>
2024-03-22 20:10:43 -0300

    Modificação do nome da pasta de RO para Analisador Lexico

---
+++ Analisador_Lexico/Operador.java
@@ -0,0 +1,68 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+import java.text.CharacterIterator;
+
+
+public class Operador extends AFD {
+    @Override
+    public Token evaluate(CharacterIterator codigo){
+        switch(codigo.current()){
+            case'+':
+                codigo.next();
+                return new Token("PLUS","+");
+            case'-':
+                codigo.next();
+                return new Token("LESS", "-");
+            case'*':
+                codigo.next();
+                return new Token("MULTI", "*");
+            case '<':
+                codigo.next();
+                if (codigo.current() == '-'){
+                    codigo.next();
+                    return new Token("ATRIBUICAO", "<-");
+                }
+                if (codigo.current() == '='){
+                    codigo.next();
+                    if(codigo.current() == '>'){
+                        codigo.next();
+                        return new Token("IGUAL", "<=>");
+                    }
+                    return new Token("MENORIGUAL", "<=");
+                }
+                return new Token("MENOR", "<");
+            case '>':
+                codigo.next();
+                if (codigo.current() == '='){
+                    codigo.next();
+                    return new Token("MAIORIGUAL", ">=");
+                }
+                return new Token("MAIOR", ">");
+            case'/':
+                codigo.next();
+                return new Token("DIV", "/");
+            case'|':
+                codigo.next();
+                return new Token("ASPAS", " \" ");
+            case')':
+                codigo.next();
+                return new Token("RPAREN",")");
+            case'(':
+                codigo.next();
+                return new Token("LPAREN","(");
+            case'}':
+                codigo.next();
+                return new Token("RCHAVE","}");
+            case'{':
+                codigo.next();
+                return new Token("LCHAVE","{");
+            case'?':
+                codigo.next();
+                return new Token("FIM", "?");
+            default:
+                return null;
+        }
+    }
+}
---
+++ Analisador_Lexico/Reservada.java
@@ -0,0 +1,77 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+import java.text.CharacterIterator;
+
+/**
+ *
+ * @author unifmursilva
+ */
+public class Reservada extends AFD{
+    @Override
+    public Token evaluate(CharacterIterator codigo){
+        if(Character.isLetter(codigo.current())){
+            String plv = readReservada(codigo);
+            if (endReservada(codigo)){
+                switch(plv){
+                    case"totum":
+                        return new Token("INT",plv);
+                    case"miseratio":
+                        return new Token("FLOAT",plv);
+                    case"reditus":
+                        return new Token("RETURN",plv);
+                    case"conditio":
+                        return new Token("BOOLEAN",plv);
+                    case"filum":
+                        return new Token("STRING",plv);
+                    case"auditio":
+                        return new Token("INPUT",plv);
+                    case"inanis":
+                        return new Token("NULL",plv);
+                    case"dicere":
+                        return new Token("PRINT",plv);
+                    case"dum":
+                        return new Token("WHILE",plv);
+                    case"propositum":
+                        return new Token("FOR",plv);
+                    case"si":
+                        return new Token("IF",plv);
+                    case"oppositum":
+                        return new Token("ELSE",plv);
+                    case"nintendum":
+                        return new Token("SWITCH",plv);
+                    default:
+                        return null;
+                }
+            }
+        }
+        return null;
+    }
+    
+    private String readReservada(CharacterIterator codigo){
+        String plv = "";
+        while (Character.isLetter(codigo.current())){
+            plv += codigo.current();
+            codigo.next();
+        }
+        return plv;
+    }
+    
+    private boolean endReservada(CharacterIterator codigo){
+        return codigo.current() == ' ' ||
+        codigo.current() == '\n' ||
+        codigo.current() == ';' ||
+        codigo.current() == '+' ||
+        codigo.current() == '-' ||
+        codigo.current() == '*' ||
+        codigo.current() == '/' ||
+        codigo.current() == '=' ||
+        codigo.current() == ')' ||
+        codigo.current() == '(' ||
+        codigo.current() == '}' ||
+        codigo.current() == '{' ||
+        codigo.current() == '.' ||
+        codigo.current() == CharacterIterator.DONE;
+    }
+}
